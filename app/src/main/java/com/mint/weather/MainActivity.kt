c664c3c570fef0f1fa8eeadb71cb6ee9a6fe1aa3
Natallia Kryukova <krunat280@gmail.com>
2021-11-03 11:55:01 +0300

    fix: get weather after request permission

--- app/src/main/java/com/mint/weather/MainActivity.kt
+++ app/src/main/java/com/mint/weather/MainActivity.kt
@@ -5,10 +5,17 @@ import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
     }
 }
--- app/src/main/java/com/mint/weather/WeatherFragment.kt
+++ app/src/main/java/com/mint/weather/WeatherFragment.kt
@@ -1,15 +1,22 @@
 package com.mint.weather
 
-import android.location.LocationManager
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.mint.weather.databinding.FragmentWeatherBinding
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
@@ -25,26 +32,38 @@ class WeatherFragment : Fragment() {
     private var _binding: FragmentWeatherBinding? = null
     private val binding get() = _binding!!
 
-    private val queue: RequestQueue by lazy { Volley.newRequestQueue(requireContext()) }
     private val weatherApi: WeatherApi by lazy { getWeatherApi("https://api.openweathermap.org") }
+
+    private var latitude: Double = 53.9
+    private var longitude: Double = 27.5667
+
+    private var fusedLocationProvider: FusedLocationProviderClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        checkLocationPermission()
+        fusedLocationProvider?.lastLocation?.addOnSuccessListener(requireActivity()) { location ->
+            if (location != null) {
+                longitude = location.longitude
+                latitude = location.latitude
+            }
+            getWeatherNow(latitude, longitude)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentWeatherBinding.inflate(inflater, container, false)
-
-
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-//        val locationManager = ContextCompat.getSystemService<Any>(requireContext().LOCATION_SERVICE) as LocationManager?
 
         binding.btnSend.setOnClickListener {
             val city = binding.editCity.text.toString().trim()
@@ -53,8 +72,92 @@ class WeatherFragment : Fragment() {
         }
     }
 
-    private fun getWeatherNow(city: City) {
-        val weatherNow: Call<WeatherNow> = weatherApi.getWeatherNow(city.lat, city.lng)
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton(
+                        "OK"
+                    ) { _, _ ->
+                        requestLocationPermission()
+                    }
+                    .create()
+                    .show()
+            } else {
+                requestLocationPermission()
+            }
+        }
+    }
+
+    private fun requestLocationPermission() {
+        requestPermissions(
+
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            MY_PERMISSIONS_REQUEST_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        fusedLocationProvider?.lastLocation?.addOnSuccessListener(requireActivity()) { location ->
+                            if (location != null) {
+                                longitude = location.longitude
+                                latitude = location.latitude
+                                getWeatherNow(latitude, longitude)
+
+                            }
+                        }
+                    }
+
+                } else {
+                    Toast.makeText(requireContext(), "permission denied", Toast.LENGTH_LONG).show()
+                    getWeatherNow(latitude, longitude)
+                    // Check if we are in a state where the user has denied the permission and
+                    // selected Don't ask again
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                            requireActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    ) {
+                        startActivity(//todo разобраться как это работает, что вызывается
+                            Intent(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                Uri.fromParts("package", requireContext().packageName, null),
+                            ),
+                        )
+                    }
+                }
+                return
+            }
+        }
+    }
+
+    private fun getWeatherNow(lat: Double, lon: Double) {
+        val weatherNow: Call<WeatherNow> = weatherApi.getWeatherNow(lat, lon)
 
         weatherNow.enqueue(object : Callback<WeatherNow> {
             override fun onResponse(call: Call<WeatherNow>, response: Response<WeatherNow>) {
@@ -76,13 +179,13 @@ class WeatherFragment : Fragment() {
 
     private fun setWeather(weatherNow: WeatherNow) {
         binding.cityName.text = weatherNow.name
-        binding.temp.text = "${weatherNow.main.temp} °C"
-        binding.feelsLike.text = "${weatherNow.main.feelsLike} °C"
-        binding.description.text = "${weatherNow.weather[0].description}"
+        binding.temp.text = "${weatherNow.main.temp.toInt()}°C"
+        binding.feelsLike.text = "Ощущается как: ${weatherNow.main.feelsLike.toInt()}°C"
+        binding.description.text = "${weatherNow.weather[0].description.replaceFirstChar { c -> c.uppercase() }}"
 
         Glide
             .with(binding.root)
-            .load("http://openweathermap.org/img/wn/${weatherNow.weather[0].icon}@2x.png")
+            .load("https://openweathermap.org/img/wn/${weatherNow.weather[0].icon}@2x.png")
             .into(binding.icon)
 
     }
@@ -97,7 +200,7 @@ class WeatherFragment : Fragment() {
                 val city = response.body()?.firstOrNull()
                 if (city != null) {
                     setValues(city.lat, city.lng)
-                    getWeatherNow(city)
+                    getWeatherNow(city.lat, city.lng)
                 }
             }
 
@@ -128,6 +231,11 @@ class WeatherFragment : Fragment() {
 
         return retrofit.create(WeatherApi::class.java)
     }
+
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_LOCATION = 1
+    }
+
 }
 
 
